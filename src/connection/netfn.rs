5ae651263905adef7100486090896aa2f9643358
datdenkikniet <jcdra1@gmail.com>
2023-04-07 16:21:55 +0200

    Add all netfns

--- src/connection/netfn.rs
+++ src/connection/netfn.rs
@@ -1,46 +1,56 @@
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum NetFn {
-    App,
-    Storage,
-    Unknown(u8),
-}
-
-impl From<u8> for NetFn {
-    fn from(value: u8) -> Self {
-        match value {
-            0x06 | 0x07 => Self::App,
-            0x0A | 0x0B => Self::Storage,
-            v => Self::Unknown(v),
+macro_rules! netfn {
+    ($($name:ident => [$req:literal | $resp:literal]),*) => {
+        #[derive(Debug, Clone, Copy, PartialEq)]
+        pub enum NetFn {
+            $($name,)*
+            Reserved(u8),
         }
-    }
-}
 
-impl NetFn {
-    pub fn request_value(&self) -> u8 {
-        match self {
-            NetFn::App => 0x06,
-            NetFn::Storage => 0x0A,
-            NetFn::Unknown(v) => {
-                if v % 2 == 1 {
-                    v - 1
-                } else {
-                    *v
+        impl From<u8> for NetFn {
+            fn from(value: u8) -> Self {
+                match value {
+                    $($req | $resp => Self::$name,)*
+                    v => Self::Reserved(v),
                 }
             }
         }
-    }
 
-    pub fn response_value(&self) -> u8 {
-        match self {
-            NetFn::App => 0x07,
-            NetFn::Storage => 0x0B,
-            NetFn::Unknown(v) => {
-                if v % 2 == 0 {
-                    v + 1
-                } else {
-                    *v
+        impl NetFn {
+            pub fn request_value(&self) -> u8 {
+                match self {
+                    $(Self::$name => $req,)*
+                    Self::Reserved(v) => {
+                        if v % 2 == 1 {
+                            v - 1
+                        } else {
+                            *v
+                        }
+                    }
+                }
+            }
+
+            pub fn response_value(&self) -> u8 {
+                match self {
+                    $(Self::$name => $resp,)*
+                    NetFn::Reserved(v) => {
+                        if v % 2 == 0 {
+                            v + 1
+                        } else {
+                            *v
+                        }
+                    }
                 }
             }
         }
-    }
+    };
 }
+
+netfn!(
+    Chassis => [0x00 | 0x01],
+    Bridge => [0x02 | 0x03],
+    SensorEvent => [0x04 | 0x05],
+    App => [0x06 | 0x07],
+    Firmware => [0x08 | 0x09],
+    Storage => [0x0A | 0x0B],
+    Transport => [0x0C | 0x0D]
+);
