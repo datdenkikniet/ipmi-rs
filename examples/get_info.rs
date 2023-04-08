769c986a74a2950cc6304f39e18de84f4aab7f84
datdenkikniet <jcdra1@gmail.com>
2023-04-08 15:37:24 +0200

    Fancy progress bar for loading SDRs

--- examples/get_info.rs
+++ examples/get_info.rs
@@ -1,9 +1,10 @@
 use std::time::Duration;
 
+use indicatif::{ProgressBar, ProgressStyle};
 use ipmi_rs::{
     app::GetDeviceId,
     connection::File,
-    sensor_event::GetSensorReading,
+    sensor_event::{GetSensorReading, ThresholdReading},
     storage::{
         record::RecordContents, GetSdrAllocInfo, GetSdrRepositoryInfo, GetSelAllocInfo,
         GetSelEntry, GetSelInfo, SdrOperation, SelCommand, SelRecordId,
@@ -14,7 +15,7 @@ use ipmi_rs::{
 fn main() {
     pretty_env_logger::init();
 
-    let file = File::new("/dev/ipmi0", Duration::from_millis(2000)).unwrap();
+    let file = File::new("/dev/ipmi0", Duration::from_millis(4000)).unwrap();
     let mut ipmi = Ipmi::new(file);
     let log_output = &LogOutput::LogTarget(log::Level::Info, "get_info".into());
     let debug_log_output = &LogOutput::LogTarget(log::Level::Debug, "get_info".into());
@@ -49,9 +50,28 @@ fn main() {
     if sdr_info.supported_ops.contains(&SdrOperation::GetAllocInfo) {
         let sdr_alloc_info = ipmi.send_recv(GetSdrAllocInfo).unwrap();
         sdr_alloc_info.log(log_output);
-    }
+    };
 
-    let sensors = ipmi.sdrs().collect::<Vec<_>>();
+    let template = "[{bar:.green/white}] {prefix} ({pos}/{len})";
+
+    let progress_bar = ProgressBar::new(sdr_info.record_count as u64)
+        .with_style(
+            ProgressStyle::with_template(&template)
+                .unwrap()
+                .progress_chars("#>-"),
+        )
+        .with_prefix("Loading SDR Records");
+
+    let sensors = ipmi
+        .sdrs()
+        .enumerate()
+        .map(|(idx, v)| {
+            progress_bar.set_position(idx as u64 + 1);
+            v
+        })
+        .collect::<Vec<_>>();
+
+    progress_bar.finish();
 
     for sensor in &sensors {
         if let RecordContents::FullSensor(full) = &sensor.contents {
@@ -59,7 +79,9 @@ fn main() {
                 .send_recv(GetSensorReading::for_sensor(full.sensor_number()))
                 .unwrap();
 
-            if let Some(display) = full.display_reading(value.reading) {
+            let reading: ThresholdReading = (&value).into();
+
+            if let Some(display) = full.display_reading(reading.reading.unwrap()) {
                 log::info!("{}: {}", full.id_string(), display);
                 sensor.log(debug_log_output);
             }
