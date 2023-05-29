7e92ec9aa0387505c3561065e0dc4b3a8cd50b55
datdenkikniet <jcdra1@gmail.com>
2023-05-29 20:53:41 +0200

    Add GetChannelAuthentication command

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,2 +1,8 @@
 mod get_device_id;
 pub use get_device_id::{DeviceId, GetDeviceId};
+
+mod get_channel_authentication_capabilities;
+pub use get_channel_authentication_capabilities::{
+    Channel, ChannelAuthenticationCapabilities, GetChannelAuthenticationCapabilities,
+    PrivilegeLevel,
+};
