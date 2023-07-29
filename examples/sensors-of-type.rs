461751b5256b6641050e37b4467e48194cdd74c1
datdenkikniet <jcdra1@gmail.com>
2023-07-29 20:41:11 +0200

    Add optional reading argument to sensors-of-type

--- examples/sensors-of-type.rs
+++ examples/sensors-of-type.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use ipmi_rs::storage::sdr::SensorType;
+use ipmi_rs::{
+    sensor_event::{GetSensorReading, ThresholdReading},
+    storage::sdr::{record::RecordContents, Record, SensorType},
+    SensorRecord,
+};
 use log::Level;
 
 mod common;
@@ -11,6 +15,9 @@ pub struct Command {
 
     #[clap(required = true)]
     types: Vec<String>,
+
+    #[clap(long, short)]
+    reading: bool,
 }
 
 fn main() -> std::io::Result<()> {
@@ -36,14 +43,42 @@ fn main() -> std::io::Result<()> {
 
     let mut ipmi = args.common.get_connection()?;
 
-    let sdrs_of_type = ipmi.sdrs().filter(|s| {
-        s.common_data()
-            .map(|c| sensor_types.contains(&c.ty))
-            .unwrap_or(false)
-    });
+    let sdrs_of_type: Vec<Record> = ipmi
+        .sdrs()
+        .filter(|s| {
+            s.common_data()
+                .map(|c| sensor_types.contains(&c.ty))
+                .unwrap_or(false)
+        })
+        .collect();
 
     for sdr in sdrs_of_type {
         ipmi_rs::Logger::log(&Level::Info.into(), &sdr);
+
+        if let RecordContents::FullSensor(full) = sdr.contents {
+            if args.reading {
+                let value = ipmi
+                    .send_recv(GetSensorReading::for_sensor(full.sensor_number()))
+                    .unwrap();
+
+                let reading: ThresholdReading = (&value).into();
+
+                if let Some(reading) = reading.reading {
+                    if let Some(display) = full.display_reading(reading) {
+                        log::info!("Current reading for {}: {}", full.id_string(), display);
+                    }
+                } else {
+                    log::warn!("No reading for {}", full.id_string());
+                }
+            }
+        } else {
+            log::warn!(
+                "Don't know how to read sensor value of non-full-record {}",
+                sdr.id()
+                    .map(|v| format!("{v}"))
+                    .unwrap_or("Unknown".to_string())
+            )
+        }
     }
 
     Ok(())
