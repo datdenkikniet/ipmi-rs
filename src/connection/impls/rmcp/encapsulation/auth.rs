7e99ee7dd79a94330ed0c670dfcec8b37050b549
datdenkikniet <jcdra1@gmail.com>
2024-01-29 20:42:10 +0100

    fix(rmcp/auth): add md2 implementation

--- src/connection/impls/rmcp/encapsulation/auth.rs
+++ src/connection/impls/rmcp/encapsulation/auth.rs
@@ -1,5 +1,7 @@
 use crate::app::auth::AuthType;
 
+use super::md2::md2;
+
 fn calculate_md5(password: &[u8; 16], session_id: u32, session_seq: u32, data: &[u8]) -> [u8; 16] {
     let mut context = md5::Context::new();
     context.consume(password);
@@ -8,6 +10,18 @@ fn calculate_md5(password: &[u8; 16], session_id: u32, session_seq: u32, data: &
     context.consume(session_seq.to_le_bytes());
     context.consume(password);
     *context.compute()
+}
+
+fn calculate_md2(password: &[u8; 16], session_id: u32, session_seq: u32, data: &[u8]) -> [u8; 16] {
+    let data = password
+        .iter()
+        .copied()
+        .chain(session_id.to_le_bytes().into_iter())
+        .chain(data.iter().copied())
+        .chain(session_seq.to_le_bytes().into_iter())
+        .chain(password.iter().copied());
+
+    md2(data)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -26,7 +40,9 @@ pub fn calculate(
 ) -> Result<Option<[u8; 16]>, CalculateAuthCodeError> {
     match (ty, password) {
         (AuthType::None, _) => Ok(None),
-        (AuthType::MD2, Some(_)) => todo!(),
+        (AuthType::MD2, Some(password)) => {
+            Ok(Some(calculate_md2(password, session_id, session_seq, data)))
+        }
         (AuthType::MD5, Some(password)) => {
             Ok(Some(calculate_md5(password, session_id, session_seq, data)))
         }
@@ -45,7 +61,11 @@ pub fn verify(
 ) -> bool {
     match (ty, password) {
         (AuthType::None, _) => true,
-        (AuthType::MD2, Some(_)) => todo!(),
+        (AuthType::MD2, Some(password)) => {
+            let calc_digest = calculate_md2(password, session_id, session_seq, data);
+
+            calc_digest == digest
+        }
         (AuthType::MD5, Some(password)) => {
             let calc_digest = calculate_md5(password, session_id, session_seq, data);
 
