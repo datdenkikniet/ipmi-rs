f72bb83ff178c82e026d9379ee47ff5dc1495dd5
Denys Fedoryshchenko <denys.f@collabora.com>
2026-01-13 22:44:41 +0200

    feat(app): add Get Channel Info command and channel type displays
    Added GetChannelInfo command + ChannelInfo parsing, defined channel
    medium/protocol/session enums with Display.
    
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- ipmi-rs-core/src/app/mod.rs
+++ ipmi-rs-core/src/app/mod.rs
@@ -3,4 +3,10 @@
 mod get_device_id;
 pub use get_device_id::{DeviceId, GetDeviceId};
 
+mod get_channel_info;
+pub use get_channel_info::{
+    AuxChannelInfo, ChannelInfo, ChannelMediumType, ChannelProtocolType, ChannelSessionSupport,
+    GetChannelInfo,
+};
+
 pub mod auth;
